344ddf8eeae22244fd0d4e4a69701b8875b685f7
Joe Brown <joseph.brown.k@gmail.com>
2024-08-28 18:24:07 -0400

    Adjusting analysis to consider constrained weights.

--- workflows/sourced/helper_functions.R
+++ workflows/sourced/helper_functions.R
@@ -3,22 +3,21 @@
 # Normalizing data to reference period
 normalize_dat <- function(data, ref_start, ref_end) {
 
-  # filter data for the reference period
+  # Filter data for the reference period
   ref_period <- subset(
     data,
-    Year >= ref_start &
-      Year <= ref_end
+    year >= ref_start &
+      year <= ref_end
   )
 
-  # calculate mean for the reference period
-  mean_ref_dat <- mean(ref_period$value, na.rm = T)
+  # Calculate the mean for the reference period
+  mean_ref_dat <- mean(ref_period$value, na.rm = TRUE)
 
-  # Normalize values by dividing each by the mean_ref_period
-  norm_dat <- data$value / mean_ref_dat
+  # Normalize values by subtracting the mean_ref_period
+  norm_dat <- data$value - mean_ref_dat
 
-  # return the new data frame
+  # Return the normalized data
   return(norm_dat)
-
 }
 
 
@@ -27,6 +26,7 @@ recode_scenarios <- function(data) {
 
   data$scenario <- recode_factor(
     data$scenario,
+    IPCC = "IPCC AR6",
     Baseline = "Baseline",
     No_Historical = "No Historical",
     No_Process = "No Process",
@@ -44,9 +44,9 @@ data_summary <- function(data){
     data %>%
     group_by(scenario) %>%
     summarize(
-      median = weighted.quantile(metric_result, w = weight_norm, probs = 0.5),
-      lower = weighted.quantile(metric_result, w = weight_norm, probs = 0.05),
-      upper = weighted.quantile(metric_result, w = weight_norm, probs = 0.95),
+      median = weighted.quantile(metric_result, w = norm_weight, probs = 0.5),
+      lower = weighted.quantile(metric_result, w = norm_weight, probs = 0.05),
+      upper = weighted.quantile(metric_result, w = norm_weight, probs = 0.95),
       .groups = "drop")
 
   return(gsat_metric_stats)
@@ -55,34 +55,34 @@ data_summary <- function(data){
 
 
 # Computing Kernal Density Estimates
-
-kde_values <- function(data, split_by, variable, weights) {
-
-  split_df <- split(data, split_by)
-
-  density_result <- lapply(names(split_df), function(df_name) {
-    # store data based on name of the current df
-    # this way we can add a scenario column to the final results
-    df <- split_df[[df_name]]
-
-    # use that df to compute density estimates weight by `weights`
-    density_values <- density(variable, weights = weights)
-
-    # build data frame with coordinates we want to plot
-    density_estimate <- data.frame(scenario = df_name,
-                                   value = density_values$x,
-                                   density = density_values$y)
-
-    return(density_estimate)
-  })
-
-  kde_values_result <- do.call(rbind, density_result)
-
-  return(kde_values_result)
-
-}
-
-kde_test <- kde_values(ecs_plot_df,
-                       split_by = ecs_plot_df$scenario,
-                       variable = ecs_plot_df$ECS,
-                       weights = ecs_plot_df$weight_norm)
+#
+# kde_values <- function(data, split_by, variable, weights) {
+#
+#   split_df <- split(data, split_by)
+#
+#   density_result <- lapply(names(split_df), function(df_name) {
+#     # store data based on name of the current df
+#     # this way we can add a scenario column to the final results
+#     df <- split_df[[df_name]]
+#
+#     # use that df to compute density estimates weight by `weights`
+#     density_values <- density(variable, weights = weights)
+#
+#     # build data frame with coordinates we want to plot
+#     density_estimate <- data.frame(scenario = df_name,
+#                                    value = density_values$x,
+#                                    density = density_values$y)
+#
+#     return(density_estimate)
+#   })
+#
+#   kde_values_result <- do.call(rbind, density_result)
+#
+#   return(kde_values_result)
+#
+# }
+#
+# kde_test <- kde_values(ecs_plot_df,
+#                        split_by = ecs_plot_df$scenario,
+#                        variable = ecs_plot_df$ECS,
+#                        weights = ecs_plot_df$weight_norm)
